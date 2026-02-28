75561162ece4e4623bbfeaffcc3bc51824e8eff9
Noor Ullah <nooullah@email.com>
2026-02-28 21:08:04 +0500

    new data

---
+++ lib/home_view.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hint: Text("search data"),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
